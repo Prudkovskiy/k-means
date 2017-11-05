a7d20b49e47f30a35750d30446214c46046abeb6
Prudkovskiy <nickolay.prud@gmail.com>
2017-11-05 12:40:34 +0300

    inception

---
+++ coverage_test.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"testing"
+)
+
+func TestMain(t *testing.T) {
+	main()
+}
+
+/*
+    to create test coverage:
+	go test -coverprofile=cover.out
+	go tool cover -html=cover.out -o cover.html
+
+*/
---
+++ main.go
@@ -0,0 +1,8 @@
+package main
+
+import "fmt"
+
+//Main do smth
+func main() {
+	fmt.Println("Hello, k-means++!")
+}
