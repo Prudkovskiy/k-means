6b12d484cc575c6507cac16e6d03bdad2c272603
Prudkovskiy <nickolay.prud@gmail.com>
2017-12-07 11:57:29 +0300

    добавлены тесты модуля init.go

--- coverage_test.go
+++ coverage_test.go
@@ -1,20 +1,56 @@
 package main
 
 import (
+	"reflect"
 	"testing"
 )
 
-func TestMain(t *testing.T) {
-	k := new(FileReader)
-	k.FileName = "in.txt"
-	b, err := k.Read()
+type TestCaseInit struct {
+	FileName string
+}
+
+func TestInitialization(t *testing.T) {
+	cases := []TestCaseInit{
+		TestCaseInit{"test_data/right.txt"},
+		TestCaseInit{"test_data/notExistFile.txt"},
+		TestCaseInit{"test_data/badYaml.txt"},
+		TestCaseInit{"test_data/badKmax.txt"},
+	}
+
+	reader := new(FileReader)
+	for caseNum, item := range cases {
+		reader.FileNameIn
+		u, err := (item.FileName)
+
+		if item.IsError && err == nil {
+			t.Errorf("[%d] expected error, got nil", caseNum)
+		}
+		if !item.IsError && err != nil {
+			t.Errorf("[%d] unexpected error", caseNum, err)
+		}
+		if !reflect.DeepEqual(u, item.User) {
+			t.Errorf("[%d] wrong results: got %+v, expected %+v",
+				caseNum, u, item.User)
+		}
+	}
+
+	reader := new(FileReader)
+	reader.FileNameIn = "test_data/in.txt"
+	bytes, err := reader.Read()
+	if err != nil {
+		t.Errorf("unexpected error: ", err)
+	}
+	err = reader.Unpack(bytes)
 	if err != nil {
 		t.Errorf("unexpected error ", err)
 	}
-	err = k.Unpack(b)
+
+	reader.FileNameIn = "notexist.txt"
+	bytes, err = reader.Read()
 	if err != nil {
-		t.Errorf("unexpected error ", err)
+		t.Errorf("expected error, it's OK! ", err)
 	}
+
 }
 
 /*
