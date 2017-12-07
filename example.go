e015c794f6c03a4db1e83f6f7a1987cd4cf339f7
Prudkovskiy <nickolay.prud@gmail.com>
2017-12-08 00:52:28 +0300

    change in example.go

--- example.go
+++ example.go
@@ -1,11 +1,13 @@
 package main
 
-import "fmt"
+import (
+	"os"
+)
 
 func main() {
 	reader := new(FileReader)
-	reader.FileNameIn = "./test_data/in2.txt"
-	reader.FileNameOut = "./test_data/out2.txt"
+	reader.FileNameIn = string(os.Args[1])  // входной файл
+	reader.FileNameOut = string(os.Args[2]) // выходной файл
 	bytes, _ := reader.Read()
 	reader.Unpack(bytes)
 
@@ -16,56 +18,68 @@ func main() {
 	pam.Kmax = reader.Kmax
 	pam.Distances = matr
 
-	clust, ff := pam.swap(1)
-	visuality := make(map[int][]int, len(clust))
-	i := 1
-	for _, val := range clust {
-		visuality[i] = make([]int, 0, len(val))
-		for _, node := range val {
-			visuality[i] = append(visuality[i], node+1)
-		}
-		i++
-	}
-	fmt.Println(visuality, ff)
+	// clust, ff := pam.swap(1)
+	// visuality := make(map[int][]int, len(clust))
+	// i := 1
+	// for _, val := range clust {
+	// 	visuality[i] = make([]int, 0, len(val))
+	// 	for _, node := range val {
+	// 		visuality[i] = append(visuality[i], node+1)
+	// 	}
+	// 	i++
+	// }
+	// fmt.Println(visuality, ff)
 
-	clust, ff = pam.swap(2)
-	visuality = make(map[int][]int, len(clust))
-	i = 1
-	for _, val := range clust {
-		visuality[i] = make([]int, 0, len(val))
-		for _, node := range val {
-			visuality[i] = append(visuality[i], node+1)
-		}
-		i++
-	}
-	fmt.Println(visuality, ff)
+	// clust, ff = pam.swap(2)
+	// visuality = make(map[int][]int, len(clust))
+	// i = 1
+	// for _, val := range clust {
+	// 	visuality[i] = make([]int, 0, len(val))
+	// 	for _, node := range val {
+	// 		visuality[i] = append(visuality[i], node+1)
+	// 	}
+	// 	i++
+	// }
+	// fmt.Println(visuality, ff)
 
-	clust, ff = pam.swap(3)
-	visuality = make(map[int][]int, len(clust))
-	i = 1
-	for _, val := range clust {
-		visuality[i] = make([]int, 0, len(val))
-		for _, node := range val {
-			visuality[i] = append(visuality[i], node+1)
-		}
-		i++
-	}
-	fmt.Println(visuality, ff)
+	// clust, ff = pam.swap(3)
+	// visuality = make(map[int][]int, len(clust))
+	// i = 1
+	// for _, val := range clust {
+	// 	visuality[i] = make([]int, 0, len(val))
+	// 	for _, node := range val {
+	// 		visuality[i] = append(visuality[i], node+1)
+	// 	}
+	// 	i++
+	// }
+	// fmt.Println(visuality, ff)
 
-	clusters, weightFunction := pam.RunPAM()
-	visuality = make(map[int][]int, len(clusters))
-	i = 1
-	for _, val := range clusters {
-		visuality[i] = make([]int, 0, len(val))
-		for _, node := range val {
-			visuality[i] = append(visuality[i], node+1)
-		}
-		i++
-	}
-	data, err := reader.Pack(visuality)
-	if err != nil {
-		panic(err)
-	}
+	// clust, ff = pam.swap(4)
+	// visuality = make(map[int][]int, len(clust))
+	// i = 1
+	// for _, val := range clust {
+	// 	visuality[i] = make([]int, 0, len(val))
+	// 	for _, node := range val {
+	// 		visuality[i] = append(visuality[i], node+1)
+	// 	}
+	// 	i++
+	// }
+	// fmt.Println(visuality, ff)
+
+	// clust, ff = pam.swap(5)
+	// visuality = make(map[int][]int, len(clust))
+	// i = 1
+	// for _, val := range clust {
+	// 	visuality[i] = make([]int, 0, len(val))
+	// 	for _, node := range val {
+	// 		visuality[i] = append(visuality[i], node+1)
+	// 	}
+	// 	i++
+	// }
+	// fmt.Println(visuality, ff)
+
+	clusters, _ := pam.RunPAM()
+	data := reader.Pack(clusters)
 	reader.Write(data)
-	fmt.Println("\n", visuality, weightFunction)
+	// fmt.Println("\n", visuality, weightFunction)
 }
